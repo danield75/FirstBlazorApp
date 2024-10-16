73012d7a66f4376bdea4a855803ff8be6c7541d2
Daniel Drion <daniel.drion@inetum-realdolmen.world>
2024-10-16 16:37:46 +0200

    Added DrawCanvas razor component (POC)

---
+++ FirstBlazorApp/wwwroot/js/drawing.js
@@ -0,0 +1,34 @@
+﻿let canvas, context;
+let drawing = false;
+let lastX = 0, lastY = 0;
+
+window.startDrawing = (x, y) => {
+    canvas = document.getElementById('drawCanvas');
+    context = canvas.getContext('2d');
+    context.lineWidth = 2;
+    context.lineCap = 'round';
+    context.strokeStyle = 'black';
+
+    drawing = true;
+    lastX = x - canvas.offsetLeft;
+    lastY = y - canvas.offsetTop;
+};
+
+window.stopDrawing = () => {
+    drawing = false;
+};
+
+window.draw = (x, y) => {
+    if (!drawing) return;
+
+    let currentX = x - canvas.offsetLeft;
+    let currentY = y - canvas.offsetTop;
+
+    context.beginPath();
+    context.moveTo(lastX, lastY);
+    context.lineTo(currentX, currentY);
+    context.stroke();
+
+    lastX = currentX;
+    lastY = currentY;
+};
