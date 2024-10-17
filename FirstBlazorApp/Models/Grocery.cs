3a926fb23dcd996c07ae3006d6b00cbc0a7eeb5f
Daniel Drion <daniel.drion@inetum-realdolmen.world>
2024-10-17 15:40:37 +0200

    Forms and validations

--- FirstBlazorApp/Models/Grocery.cs
+++ FirstBlazorApp/Models/Grocery.cs
@@ -1,8 +1,14 @@
-﻿namespace FirstBlazorApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstBlazorApp.Models
 {
 	public class Grocery
 	{
+		[Required]
+		[StringLength(15, ErrorMessage = "Name should be less than 15 characters.")]
 		public string Name { get; set; }
+		[Required]
+		[Range(1, 10000, ErrorMessage = "Valid price range is between 1 and 10000.")]
 		public float Price { get; set; }
 	}
 }
